8ff9a653aa2ec6c98f03424b3572371e95ab18db
Manas Saloi <manassaloi@gmail.com>
2024-12-30 20:10:37 +0530

    Update footer-loader.js

--- js/footer-loader.js
+++ js/footer-loader.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('/includes/footer.html')
+    fetch('includes/footer.html')
         .then(response => response.text())
         .then(data => {
             document.body.insertAdjacentHTML('beforeend', data);
